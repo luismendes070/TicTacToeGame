294a0a9324703624cb4a45ff113e32baf2ac3f21
Luis Mendes <machadoluismendes@gmail.com>
2023-09-03 11:02:51 -0300

    Increment
    Failed to instantiate test runner class androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
    
    java.lang.RuntimeException: Failed to instantiate test runner class androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.github.luismendes070.tictactoegame"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.2"
+        versionCode = 2
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/test/java/com/github/luismendes070/tictactoegame/MyActivityTest.kt
+++ app/src/test/java/com/github/luismendes070/tictactoegame/MyActivityTest.kt
@@ -1,8 +1,18 @@
 package com.github.luismendes070.tictactoegame
 
+// import java.util.regex.Pattern.matches
+// import androidx.core.content.MimeTypeFilter.matches
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 // Bard
 @RunWith(AndroidJUnit4::class)
